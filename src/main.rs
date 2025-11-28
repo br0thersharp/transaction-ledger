0f44f1ca1392de75e002d27d7eee21920fb522b3
Erez Semaria <erez.semaria@gmail.com>
2025-11-27 22:41:52 -0500

    add csv reader, project types, and main errors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs::File;
+use transactions_ledger::io::csv_reader::read_csv;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input_path = env::args()
+        .nth(1)
+        .expect("usage: transaction-manager <input.csv>");
+    let file = File::open(input_path)?;
+    Ok(())
 }
